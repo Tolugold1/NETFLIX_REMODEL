00112ae8077b17edbfad45b731683ef3d25dddcc
Tolulope1 <toluadelekesteve@gmail.com>
2022-10-01 23:38:44 +0100

    update

--- src/Components/Movies/MovieCarousel.js
+++ src/Components/Movies/MovieCarousel.js
@@ -3,6 +3,7 @@ import "./MovieCarousel.styles.scss";
 import { movies } from "./imageData";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import $ from 'jquery';
+import { Link } from "react-router-dom";
 
 const MovieCarousel= () => {
 
@@ -36,36 +37,51 @@ const MovieCarousel= () => {
         <div className="movie-component">
             <div className='iconss'>
                 <MdKeyboardArrowLeft className="left" onClick={leftHandler}/>
-                <MdKeyboardArrowRight className='right' onClick={rightHandler}/></div>
+                <MdKeyboardArrowRight className='right' onClick={rightHandler}/>
+            </div>
             <div className="movie-inner-component">
-                <div className="movieImgTxt">
-                    <img src={movies[0].image} alt={movies[0].Action} className="movie_image"/>
-                    <h3>{movies[0].Action}</h3>
-                </div>
-                <div className="movieImgTxt">
-                    <img src={movies[1].image} alt={movies[1].Action} className="movie_image"/>
-                    <h3>{movies[1].Action}</h3>  
-                </div>
-                <div className="movieImgTxt">
-                    <img src={movies[2].image} alt={movies[2].Action} className="movie_image"/>
-                    <h3>{movies[2].Action}</h3>
-                </div>
-                <div className="movieImgTxt">
-                    <img src={movies[3].image} alt={movies[3].Action} className="movie_image"/>
-                    <h3>{movies[3].Action}</h3>
-                </div>
-                <div className="movieImgTxt">
-                    <img src={movies[4].image} alt={movies[4].Action} className="movie_image"/>
-                    <h3>{movies[4].Action}</h3>
-                </div>
-                <div className="movieImgTxt">
-                    <img src={movies[5].image} alt={movies[5].Action} className="movie_image"/>
-                    <h3>{movies[5].Action}</h3> 
-                </div>
-                <div className="movieImgTxt">
-                    <img src={movies[6].image} alt={movies[6].Action} className="movie_image"/>
-                    <h3>{movies[6].Action}</h3>
-                </div>
+                <Link to="/signin">
+                    <div className="movieImgTxt">
+                        <img src={movies[0].image} alt={movies[0].Action} className="movie_image"/>
+                        <h3>{movies[0].Action}</h3>
+                    </div>
+                </Link>
+                <Link to="">
+                    <div className="movieImgTxt">
+                        <img src={movies[1].image} alt={movies[1].Action} className="movie_image"/>
+                        <h3>{movies[1].Action}</h3>  
+                    </div>
+                </Link>
+                <Link to="">
+                    <div className="movieImgTxt">
+                        <img src={movies[2].image} alt={movies[2].Action} className="movie_image"/>
+                        <h3>{movies[2].Action}</h3>
+                    </div>
+                </Link>
+                <Link to="">
+                    <div className="movieImgTxt">
+                        <img src={movies[3].image} alt={movies[3].Action} className="movie_image"/>
+                        <h3>{movies[3].Action}</h3>
+                    </div>
+                </Link>
+                <Link to="">
+                    <div className="movieImgTxt">
+                        <img src={movies[4].image} alt={movies[4].Action} className="movie_image"/>
+                        <h3>{movies[4].Action}</h3>
+                    </div>
+                </Link>
+                <Link to="">
+                    <div className="movieImgTxt">
+                        <img src={movies[5].image} alt={movies[5].Action} className="movie_image"/>
+                        <h3>{movies[5].Action}</h3> 
+                    </div>
+                </Link>
+                <Link to="">
+                    <div className="movieImgTxt">
+                        <img src={movies[6].image} alt={movies[6].Action} className="movie_image"/>
+                        <h3>{movies[6].Action}</h3>
+                    </div>
+                </Link>
             </div>
         </div>
     )
